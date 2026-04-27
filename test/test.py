fd79bf92f2db0b14d2a0be7243d6a61b31b17899
felipe fermino <noobkillerbr21@gmail.com>
2026-04-27 02:03:55 -0300

    testepython1

--- test/test.py
+++ test/test.py
@@ -1,45 +1,32 @@
 from src.main import *
 from unittest.mock import patch
 
-
 def test_root():
     result = root()
-    yield result
     assert result == {"message": "Hello World"}
 
-
 def test_funcaoteste():
-    with patch('ramdom.randint', return_value=12345):
+    with patch('random.randint', return_value=12345):
         result = funcaoteste()
-        yield result
-
-    assert result == {"teste": True, "num_aleatorio": 12345}
-
+        assert result == {"teste": True, "num_aleatorio": 12345}
 
 def test_create_estudante():
     estudante_teste = Estudante(name="Fulano", curso="Curso 1", ativo=False)
     result = create_estudante(estudante_teste)
-    yield result
     assert estudante_teste == result
 
 def test_update_estudante_negativo():
     result = update_estudante(-5)
-    yield result
     assert not result
-
 
 def test_update_estudante_positivo():
     result = update_estudante(10)
-    yield result
     assert result
 
 def test_delete_estudante_negativo():
     result = delete_estudante(-5)
-    yield result
     assert not result
-
 
 def test_delete_estudante_positivo():
     result = delete_estudante(5)
-    yield result
     assert result
